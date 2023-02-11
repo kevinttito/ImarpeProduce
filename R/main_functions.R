30c45272a2851e365e81d5a218fcf127e2777d5c
Hans Kevin Ttito Sanchez <hanskevin26@gmail.com>
2023-02-11 10:24:33 -0500

    "new functions"

--- R/main_functions.R
+++ R/main_functions.R
@@ -17,30 +17,48 @@
 # Faenas ------------------------------------------------------------------
 
 
-read_faenas = function(path,pattern = ".csv",...) {
+read_faenas = function(path, pattern = ".csv",...) {
 
   list_faenas = list.files(path = path,pattern = pattern, full.names = TRUE, ...)
 
-  tablas_faenas = lapply(list_faenas, read.csv, skip = 4, fileEncoding = "latin1", stringsAsFactors = FALSE)
+  tablas_faenas = lapply(list_faenas, read.csv, skip = 5, fileEncoding = "latin1", stringsAsFactors = FALSE, header = FALSE)
 
-  xd = length(tablas_faenas)
+  tablas_faenas = do.call("rbind",tablas_faenas)
 
-  new_data_faenas = tablas_faenas[[xd]]
+  names(tablas_faenas) = c("id", "x1","armador","vessel","x2","x3","id_vessel","start_port","start_date","end_date","id_faena","n_calas","status","origin","record_date")
 
-  xd = xd - 1
-  while (xd > 1) {
+  tablas_faenas = tablas_faenas[,c("armador","vessel","id_vessel","start_port","start_date","end_date","id_faena","n_calas","status","origin","record_date")]
 
-    new_data_faenas = rbind(new_data_faenas,tablas_faenas[[xd]])
-    # print(dim(new_data_faenas))
-    new_data_faenas = new_data_faenas[!duplicated(new_data_faenas[,c("Embarcación","Matrícula","Punto.de.Zarpe","Fecha.Inicio", "Codigo.Faena","Fecha.Registro")]),]
-    xd = xd - 1
-  }
+  tablas_faenas = tablas_faenas[!duplicated(tablas_faenas[,c("vessel","id_vessel","start_port","start_date", "id_faena","record_date")]),]
 
-  new_data_faenas =  new_data_faenas[,c("Armador","Embarcación","Matrícula","Punto.de.Zarpe","Fecha.Inicio","Fecha.Fin","Codigo.Faena","N..Calas","Estado","Origen","Fecha.Registro")]
-
-  new_data_faenas = new_data_faenas[!duplicated(new_data_faenas[,c("Armador","Embarcación","Matrícula","Punto.de.Zarpe","Fecha.Inicio","Fecha.Fin","Codigo.Faena","N..Calas","Estado","Origen","Fecha.Registro")]),]
-
-
-  return(new_data_faenas)
+  return(tablas_faenas)
 
 }
+
+
+# Calas -------------------------------------------------------------------
+
+
+read_calas = function(path, pattern = ".csv",...) {
+
+  list_calas = list.files(path = path,pattern = pattern, full.names = TRUE, ...)
+
+
+  tablas_calas = lapply(list_calas, read.csv, skip = 5, fileEncoding = "latin1", stringsAsFactors = FALSE, header = FALSE)
+
+  tablas_calas = do.call("rbind", tablas_calas)
+
+  names(tablas_calas) = c("id", "x1","id_faena","n_cala","start_date","x2","x3","end_date","start_lat","start_lon","end_lat","end_lon","gear","description","catch_kg","status","origin","record_date")
+
+  tablas_calas$description =  str_replace_all(string = tablas_calas$description,pattern = "\\p{WHITE_SPACE}",replacement = "")
+
+  tablas_calas = tablas_calas[,c("id_faena","n_cala","start_date","end_date","start_lat","start_lon","end_lat","end_lon","gear","description","catch_kg","status","origin","record_date")]
+
+  tablas_calas = tablas_calas[!duplicated(tablas_calas[,c("id_faena","n_cala","start_date","end_date","start_lat","start_lon","end_lat","end_lon","gear","description","catch_kg","status","origin","record_date")]),]
+
+  return(tablas_calas)
+
+}
+
+
+faenas = read_calas(path = "D:/Pesca_anchoveta/Pesca_anchoveta_2022_II_nc/produce/data/calas/",pattern = ".csv")
